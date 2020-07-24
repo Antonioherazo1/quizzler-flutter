0ccc1e664296e8abf8ad69b89974b867fb9852cd
Antonio Herazo <antonioherazo7@hotmail.com>
2020-07-24 09:06:58 -0500

    primer commit

--- lib/main.dart
+++ lib/main.dart
@@ -1,4 +1,5 @@
 import 'package:flutter/material.dart';
+import 'package:rflutter_alert/rflutter_alert.dart';
 
 void main() => runApp(Quizzler());
 
@@ -25,6 +26,52 @@ class QuizPage extends StatefulWidget {
 }
 
 class _QuizPageState extends State<QuizPage> {
+  int n = 0;
+  List<Icon> scoreKeeper = [];
+
+  void resetGame() {
+    Alert(
+      context: context,
+      title: 'Finished!',
+      desc: 'You\'ve reached the end of the quiz.',
+    ).show();
+    scoreKeeper = [];
+    n = 0;
+  }
+
+  List<List<String>> questions = [
+    ['You can lead a cow down stairs but not up stairs.', 'false'],
+    ['Approximately one quarter of human bones are in the feet.', 'true'],
+    ['A slug\'s blood is green.', 'true'],
+    ['Buzz Aldrin\'s mother\'s maiden name was \"Moon\".', 'true'],
+    ['It is illegal to pee in the Ocean in Portugal.', 'true'],
+    [
+      'No piece of square dry paper can be folded in half more than 7 times.',
+      'false'
+    ],
+    [
+      'In London, UK, if you happen to die in the House of Parliament, you are technically entitled to a state funeral, because the building is considered too sacred a place.',
+      'true'
+    ],
+    [
+      'The loudest sound produced by any animal is 188 decibels. That animal is the African Elephant.',
+      'false'
+    ],
+    [
+      'The total surface area of two human lungs is approximately 70 square metres.',
+      'true'
+    ],
+    ['Google was originally called \"Backrub\".', 'true'],
+    [
+      'Chocolate affects a dog\'s heart and nervous system; a few ounces are enough to kill a small dog.',
+      'true'
+    ],
+    [
+      'In West Virginia, USA, if you accidentally hit an animal with your car, you are free to take it home to eat.',
+      'true'
+    ]
+  ];
+
   @override
   Widget build(BuildContext context) {
     return Column(
@@ -37,7 +84,7 @@ class _QuizPageState extends State<QuizPage> {
             padding: EdgeInsets.all(10.0),
             child: Center(
               child: Text(
-                'This is where the question text will go.',
+                questions[n][0],
                 textAlign: TextAlign.center,
                 style: TextStyle(
                   fontSize: 25.0,
@@ -61,7 +108,24 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked true.
+                setState(() {
+                  if (questions[n][1] == 'true') {
+                    scoreKeeper.add(Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ));
+                  } else {
+                    scoreKeeper.add(Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ));
+                  }
+                  if (n < questions.length - 1) {
+                    n++;
+                  } else {
+                    resetGame();
+                  }
+                });
               },
             ),
           ),
@@ -79,19 +143,32 @@ class _QuizPageState extends State<QuizPage> {
                 ),
               ),
               onPressed: () {
-                //The user picked false.
+                setState(() {
+                  if (questions[n][1] == 'false') {
+                    scoreKeeper.add(Icon(
+                      Icons.check,
+                      color: Colors.green,
+                    ));
+                  } else {
+                    scoreKeeper.add(Icon(
+                      Icons.close,
+                      color: Colors.red,
+                    ));
+                  }
+                  if (n < questions.length - 1) {
+                    n++;
+                  } else {
+                    resetGame();
+                  }
+                });
               },
             ),
           ),
         ),
-        //TODO: Add a Row here as your score keeper
+        Row(
+          children: scoreKeeper,
+        )
       ],
     );
   }
 }
-
-/*
-question1: 'You can lead a cow down stairs but not up stairs.', false,
-question2: 'Approximately one quarter of human bones are in the feet.', true,
-question3: 'A slug\'s blood is green.', true,
-*/
